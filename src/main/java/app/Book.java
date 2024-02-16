3c063e6f64bfc09c14e27d6a6f29f89dae3ca56e
TaiKie <tentingsanook@gmail.com>
2024-02-16 23:28:14 +0700

    test Book insert name

---
+++ src/main/java/app/Book.java
@@ -0,0 +1,22 @@
+package app;
+
+public class Book {
+    private String nameBook;
+    private String nameWriter;
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public String getNameWriter() {
+        return nameWriter;
+    }
+
+    public void setNameWriter(String nameWriter) {
+        this.nameWriter = nameWriter;
+    }
+}
