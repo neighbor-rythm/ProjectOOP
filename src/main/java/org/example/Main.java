bf7a42981f294228c5107fdee3069883377caa56
Nawa-Ong <newngang@gmail.com>
2024-02-16 15:36:48 +0700

    TestingV.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("Hello world!");
-        System.out.println(":D");
+        System.out.println(":DDDD");
     }
 }
