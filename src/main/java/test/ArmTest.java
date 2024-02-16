d0ab57d01012dd0bad4b6a1d16b505480f8260fa
JokePed <armjetnipat@gmail.com>
2024-02-16 22:25:16 +0700

    method sql (localhost)

--- src/main/java/test/ArmTest.java
+++ src/main/java/test/ArmTest.java
@@ -3,10 +3,31 @@ package test;
 import db.LibearchDB;
 
 import java.sql.*;
+import  java.util.*;
 
 public class ArmTest {
 
-    public static void main(String[] args) {
+    public static void insertDB(String email, String password) {
+        Connection connection = LibearchDB.getConnection();
+        try {
+            String sql = "INSERT INTO users (email, password) VALUES ('" + email + "', '" + password + "')";
+            Statement statement = connection.createStatement();
+            int result = statement.executeUpdate(sql);
+            queryDB();
+        } catch (SQLException e) {
+            System.err.println("SQL error:" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.err.println("SQL closing error:" + e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static void queryDB() {
         Connection connection = LibearchDB.getConnection();
         try {
             String sql = "SELECT * FROM users";
@@ -28,6 +49,50 @@ public class ArmTest {
                     System.err.println("SQL closing error:" + e.getMessage());
                 }
             }
+        }
+    }
+
+    public static void updateDB(String email, String new_password) {
+        Connection connection = LibearchDB.getConnection();
+        try {
+            String sql = "UPDATE users SET password = '"+ new_password + "' WHERE email = '" + email + "';";
+            Statement statement = connection.createStatement();
+            int result = statement.executeUpdate(sql);
+            queryDB();
+        } catch (SQLException e) {
+            System.err.println("SQL error:" + e.getMessage());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.err.println("SQL closing error:" + e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Method (insert/update/query): ");
+        String method = sc.nextLine();
+        switch (method) {
+            case "insert" -> {
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                System.out.print("Password: ");
+                String password = sc.nextLine();
+                insertDB(email, password);
+            }
+            case "query" -> queryDB();
+            case "update" -> {
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                System.out.print("New password: ");
+                String new_password = sc.nextLine();
+                updateDB(email, new_password);
+            }
+            default -> System.out.println("Invalid method!!");
         }
 
     }
